e42c0abed6353798edcbea0d0dc1accdd7797894
hyeon-z <phj8221@naver.com>
2024-02-01 18:01:36 +0900

    🎉 프로젝트 세팅

---
+++ src/main/java/org/packman/PackmanApplication.java
@@ -0,0 +1,13 @@
+package org.packman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackmanApplication.class, args);
+	}
+
+}
