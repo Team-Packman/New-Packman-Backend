955524dd80e5673be2c22200180502b4b32f98da
Park Hyeon Ji <63945197+hyeon-z@users.noreply.github.com>
2024-03-04 02:06:48 +0900

    패킹리스트 목록 조회 API 구현 (#12)

---
+++ src/main/java/org/packman/user/UserRepository.java
@@ -0,0 +1,9 @@
+package org.packman.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
