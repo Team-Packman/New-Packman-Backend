2198f0de513978e6e8b406252719ec54c45ccabf
hyeonz <phj8221@naver.com>
2024-03-04 01:53:34 +0900

    ✨ 패킹리스트 순서 변경 API 구현

--- src/main/java/org/packman/packingList/PackingList.java
+++ src/main/java/org/packman/packingList/PackingList.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -15,4 +16,39 @@ public class PackingList {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private Integer position;
+
+    private String title;
+
+    private Long userId;
+
+    @Builder
+    public PackingList(Long id, Integer position, String title, Long userId) {
+        this.id = id;
+        this.position = position;
+        this.title = title;
+        this.userId = userId;
+    }
+
+    public PackingList updatePosition(Integer position) {
+        return PackingList.builder()
+                .id(this.id)
+                .position(position)
+                .title(this.title)
+                .userId(this.userId)
+                .build();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
+
--- src/main/java/org/packman/packingList/PackingListService.java
+++ src/main/java/org/packman/packingList/PackingListService.java
@@ -2,22 +2,77 @@ package org.packman.packingList;
 
 import org.packman.category.Category;
 import org.packman.category.CategoryService;
+import org.packman.packingList.request.PackingListPosition;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@Transactional(readOnly = true)
 public class PackingListService {
+
+    private final PackingListRepository packingListRepository;
 
     private final CategoryService categoryService;
 
-    public PackingListService(CategoryService categoryService) {
+    public PackingListService(PackingListRepository packingListRepository, CategoryService categoryService) {
+        this.packingListRepository = packingListRepository;
         this.categoryService = categoryService;
     }
 
+    @Transactional(readOnly = true)
     public List<Category> getCategories(Long packingListId) {
         return categoryService.getAllByPackingListId(packingListId);
+    }
+
+    @Transactional
+    public void updatePosition(PackingListPosition packingListPosition) {
+        PackingList originalPackingList = packingListRepository.findById(packingListPosition.getId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 패킹리스트입니다."));
+
+        packingListOut(originalPackingList);
+
+        PackingList updatePackingList = originalPackingList.updatePosition(
+                packingListPosition.getPosition()
+        );
+
+        packingListIn(updatePackingList);
+    }
+
+    public void packingListOut(PackingList packingListBeforeMove) {
+        List<PackingList> packingLists = packingListRepository.findByPositionGreaterThan(
+                packingListBeforeMove.getPosition()
+        );
+
+        List<PackingList> updatedPackingLists = new ArrayList<>();
+
+        for (PackingList packingList : packingLists) {
+            PackingList updatePackingList = packingList.updatePosition(packingList.getPosition() - 1);
+            updatedPackingLists.add(updatePackingList);
+        }
+
+        PackingList originalPackingList = packingListBeforeMove.updatePosition(null);
+
+        updatedPackingLists.add(originalPackingList);
+
+        packingListRepository.saveAll(updatedPackingLists);
+    }
+
+    private void packingListIn(PackingList packingListAfterMove) {
+        List<PackingList> packingLists = packingListRepository.findByPositionGreaterThanEqual(
+                packingListAfterMove.getPosition()
+        );
+
+        List<PackingList> updatePacks = new ArrayList<>();
+
+        for (PackingList packingList : packingLists) {
+            PackingList updatePack = packingList.updatePosition(packingList.getPosition() + 1);
+            updatePacks.add(updatePack);
+        }
+
+        updatePacks.add(packingListAfterMove);
+
+        packingListRepository.saveAll(updatePacks);
     }
 }
