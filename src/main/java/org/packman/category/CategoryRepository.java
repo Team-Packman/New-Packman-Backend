65283119e374c69424b18971a4ab6e4caadfea2b
hyeonz <phj8221@naver.com>
2024-06-06 02:33:17 +0900

    🚚: 카테고리 관련 폴더 정리

--- src/main/java/org/packman/category/CategoryRepository.java
+++ src/main/java/org/packman/category/CategoryRepository.java
@@ -1,5 +1,6 @@
 package org.packman.category;
 
+import org.packman.category.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
---
+++ src/main/java/org/packman/category/domain/Category.java
@@ -0,0 +1,29 @@
+package org.packman.category.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Long packingListId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
