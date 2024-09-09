2fd8efc69c7355af4e6f1ca1ff59aaa7d37700dd
hyeonz <phj8221@naver.com>
2024-09-09 17:22:47 +0900

    ✨카테고리 생성 API 구현

--- src/main/java/org/packman/category/CategoryController.java
+++ src/main/java/org/packman/category/CategoryController.java
@@ -2,12 +2,18 @@ package org.packman.category;
 
 import org.packman.pack.domain.Pack;
 import org.packman.pack.dto.response.PackGet;
+import org.packman.packingList.dto.response.PackingListCreate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.packman.packingList.domain.Type.ALONE;
+import static org.packman.packingList.domain.Type.TOGETHER;
 
 @RestController
 @RequestMapping("/api/v2/categories")
@@ -17,6 +23,11 @@ public class CategoryController {
 
     private CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    public void create(@RequestBody CategoryCreate request) {
+        categoryService.create(request.toEntity());
     }
 
     @GetMapping("/{categoryId}/packs")
--- src/main/java/org/packman/category/domain/Category.java
+++ src/main/java/org/packman/category/domain/Category.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,6 +24,35 @@ public class Category {
     @Getter
     private Integer position;
 
+    @Getter
     private Long packingListId;
+
+    @Builder
+    private Category(
+            Long id,
+            String name,
+            Integer position,
+            Long packingListId
+    ) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.packingListId = packingListId;
+    }
+
+    public CategoryBuilder category() {
+        return new CategoryBuilder()
+                .id(this.id)
+                .name(this.name)
+                .position(this.position)
+                .packingListId(this.packingListId);
+    }
+
+    public Category updatePosition(Integer position) {
+        return category()
+                .id(null)
+                .position(position)
+                .build();
+    }
 
 }
